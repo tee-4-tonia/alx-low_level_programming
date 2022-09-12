e8a65ade7a56b54e8a6af8238ce8ddbd5ae36149
tee-4-tonia <ebubetonia2018@gmail.com>
2022-09-12 04:49:40 -0700

    lo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point to the program
+ *
+ * Return: return 0 when successful
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
