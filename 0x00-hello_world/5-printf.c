3f85a42de0355c66a83359a6e240b06bcfedfb23
tee-4-tonia <ebubetonia2018@gmail.com>
2022-09-12 04:54:44 -0700

    ok

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *
  * Return: return 0 when successful
  */
-int main (void)
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+			return(0);
 }
